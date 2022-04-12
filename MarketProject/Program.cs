e72b78c7fd23f8c30361df1b50f9d1fc9553fdc5
Harel Afriat <harelaf@post.bgu.ac.il>
2022-04-12 14:00:15 +0300

    Initial project commit

---
+++ MarketProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MarketProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
