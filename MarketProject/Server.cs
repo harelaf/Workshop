fc2002b79023d212400dc14a3ff118f2b6e3c494
אפיק מלכה <Afikmal@post.bgu.ac.il>
2022-04-21 16:33:55 +0300

    edit main

---
+++ MarketProject/Server.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Collections.Concurrent;
+
+namespace MarketProject
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
