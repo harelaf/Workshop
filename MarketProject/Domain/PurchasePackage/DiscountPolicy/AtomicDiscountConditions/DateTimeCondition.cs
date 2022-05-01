0da25d11cbb11bb6667f0be34a634b64f226c01d
RonMoshe <moshr@post.bgu.ac.il>
2022-05-01 17:47:22 +0300

    Add and implements DateTime conditions: Hour and DayOfWeek conditions

---
+++ MarketProject/Domain/PurchasePackage/DiscountPolicy/AtomicDiscountConditions/DateTimeCondition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarketProject.Domain.PurchasePackage.DiscountPolicy
+{
+    public abstract class DateTimeCondition : AtomicDiscountCondition
+    {
+        protected DateTime _now;
+        
+        public DateTimeCondition()
+        {
+            _now = DateTime.Now;
+        }
+    }
+}
---
+++ MarketProject/Domain/PurchasePackage/DiscountPolicy/AtomicDiscountConditions/DayOnWeekCondition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarketProject.Domain.PurchasePackage.DiscountPolicy.AtomicDiscountConditions
+{
+    public class DayOnWeekCondition : DateTimeCondition
+    {
+        protected DayOfWeek _dayOnWeek; //between 
+        
+        public DayOnWeekCondition(String day) : base()
+        {
+            _dayOnWeek = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), day);
+        }
+        public override bool Check()
+        {
+            return _now.DayOfWeek == _dayOnWeek;
+        }
+    }
+}
