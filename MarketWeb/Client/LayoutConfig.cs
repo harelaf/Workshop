b2e32e705bb4506897b9f91c11cdd6189a124273
אפיק מלכה <Afikmal@post.bgu.ac.il>
2022-05-13 17:49:55 +0300

    don't push

---
+++ MarketWeb/Client/LayoutConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace MarketWeb.Client
+{
+    public static class LayoutConfig
+    {
+        public static bool IsGuest { get; set; } = true;
+        public static bool IsLoggedIn { get; set; } = false;
+        public static bool IsAdmin { get; set; } = false;
+
+    }
+}
