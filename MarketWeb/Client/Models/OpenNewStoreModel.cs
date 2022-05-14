9ca059feb3201d235c81148f618434568ce129fc
אפיק מלכה <Afikmal@post.bgu.ac.il>
2022-05-14 23:56:21 +0300

    add pages: myMessafes, MyPurchesHistory, OpenNewStore, Purchase

---
+++ MarketWeb/Client/Models/OpenNewStoreModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace MarketWeb.Client.Models
+{
+    public class OpenNewStoreModel
+    {
+        [Required]
+        public string storeName { get; set; }
+
+    }
+}
---
+++ MarketWeb/Client/Models/PurchaseModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MarketWeb.Client.Models
+{
+    public class PurchaseModel
+    {
+        [Required]
+        public string address { get; set; }
+
+        [Required]
+        public string city { get; set; }
+        [Required]
+        public string country { get; set; }
+        [Required]
+        public string zip { get; set; }
+        [Required]
+        public string purchaserName { get; set; }
+        [Required]
+        public string paymentMethode { get; set; }
+        [Required]
+        public string shipmentMethode { get; set; }
+
+    }
+}
