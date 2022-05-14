45e9fb24565b82c29afaba26f0cbcdcc50f4c6ef
Harel Afriat <harelaf@post.bgu.ac.il>
2022-05-15 00:13:11 +0300

    Converted some pages from old GUI to blazor-style

---
+++ MarketWeb/Client/Models/AddReviewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MarketWeb.Client.Models
+{
+    public class AddReviewModel
+    {
+        [Required]
+        public int Rating { get; set; } = 5;
+
+        public string Review { get; set; }
+    }
+}
