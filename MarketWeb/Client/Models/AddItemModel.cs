215b9dbf57a314f2ff5bcc1f4b811052dde381db
OrenP_BGU <orenpa@post.bgu.ac.il>
2022-05-15 08:53:07 +0300

    Improved store

---
+++ MarketWeb/Client/Models/AddItemModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MarketWeb.Client.Models
+{
+    public class AddItemModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [Range(0, 10000000)]
+        public float Price { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        [Range(0,10000000)]
+        public int Quantity { get; set; }
+
+    }
+}
---
+++ MarketWeb/Client/Models/UpdateQuantityModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MarketWeb.Client.Models
+{
+    public class UpdateQuantityModel
+    {
+        [Required]
+        public int ItemID { get; set; }
+        [Required]
+        [Range(0,1000000)]
+        public int Quantity { get; set; }
+    }
+}
