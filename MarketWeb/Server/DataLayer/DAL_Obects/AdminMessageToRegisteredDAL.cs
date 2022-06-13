81504af82b05206b5fcc5c3920700f45d0d05fe4
אפיק מלכה <Afikmal@post.bgu.ac.il>
2022-06-14 00:14:37 +0300

    commit

--- MarketWeb/Server/DataLayer/DAL_Obects/AdminMessageToRegisteredDAL.cs
+++ MarketWeb/Server/DataLayer/DAL_Obects/AdminMessageToRegisteredDAL.cs
@@ -16,18 +16,16 @@ namespace MarketWeb.Server.DataLayer
         [Required]
         public String _message { get; set; }
 
-        public AdminMessageToRegisteredDAL(int mid, string receiverUsername, string senderUsername, string title, string message)
+        public AdminMessageToRegisteredDAL(int mid, string senderUsername, string title, string message)
         {
             this.mid = mid;
-            _receiverUsername = receiverUsername;
             _senderUsername = senderUsername;
             _title = title;
             _message = message;
         }
 
-        public AdminMessageToRegisteredDAL(string receiverUsername, string senderUsername, string title, string message)
+        public AdminMessageToRegisteredDAL(string senderUsername, string title, string message)
         {
-            _receiverUsername = receiverUsername;
             _senderUsername = senderUsername;
             _title = title;
             _message = message;
