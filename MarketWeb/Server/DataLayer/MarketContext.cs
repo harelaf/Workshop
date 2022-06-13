14c5130ed4c603a7b25ee613f61a40da9da17e28
Harel Afriat <harelaf@post.bgu.ac.il>
2022-06-13 18:30:13 +0300

    Deleted additional connection strings

--- MarketWeb/Server/DataLayer/MarketContext.cs
+++ MarketWeb/Server/DataLayer/MarketContext.cs
@@ -11,8 +11,6 @@ namespace MarketWeb.Server.DataLayer
         public DbSet<StorePurchasedBasketDAL> StorePurchaseHistory { get; set; }
         public DbSet<RegisteredPurchasedCartDAL> RegisteredPurchaseHistory { get; set; }
         public string connectionStr { get; set; } = "Data Source=34.159.230.231;User Id=sqlserver;Password=WorkshopSadna20a;"; //Encrypt=True;TrustServerCertificate=True;MultipleActiveResultSets=True";
-        //public string connectionStr { get; set; } = "Server=tcp:127.0.0.1,1443;Database=bgu-se-workshop-20a;Integrated Security=False;User Id=sqlserver;Password=WorkshopSadna20a;Encrypt=True;TrustServerCertificate=True;MultipleActiveResultSets=True";
-        //public string connectionStr { get; set; } = @"Server=(localdb)\MSSQLLocalDB;Database=TestDB;Trusted_Connection=True;";
 
         // The following configures EF to create a Sqlite database file in the
         // special "local" folder for your platform.
